8ccdffe3d1a19475e45f0c58a59083537938369b
Jemuel Balangcod <jemuelb@gmail.com>
2021-07-11 08:39:42 -0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "BIBAK San Diego",
+  },
+  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-gatsby-cloud"],
+};
