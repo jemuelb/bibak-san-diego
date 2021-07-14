62c531f584ee938e864ed0557c2bad0902dac0cb
Jemuel Balangcod <jemuelb@gmail.com>
2021-07-14 04:38:25 -0700

    Updated siteurl variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
+    siteUrl: "https://www.bibaksandiego.org",
     title: "BIBAK San Diego",
   },
   plugins: ["gatsby-plugin-emotion", "gatsby-plugin-gatsby-cloud"],
