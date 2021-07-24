1261d2eef58310acf52f4ea52680f6f37600d4d3
Jemuel Balangcod <jemuelb@gmail.com>
2021-07-24 13:17:57 -0700

    Refactored header out of layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,10 @@
 import * as React from 'react'
-import {Link, useStaticQuery, graphql} from 'gatsby'
+import {useStaticQuery, graphql} from 'gatsby'
+import Header from './header'
+
+const mainClass = [
+    "container"
+]
 
 const Layout = ({pageTitle, children}) => {
     const data = useStaticQuery(graphql`
@@ -15,27 +20,11 @@ const Layout = ({pageTitle, children}) => {
     return (
         <main>
             <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-            <p>{data.site.siteMetadata.title}</p>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link
-                            to="https://www.facebook.com/groups/117966411709158/"
-                        >
-                            Facebook Group
-                        </Link>
-                    </li>
-
-                </ul>
-            </nav>
-            <h1>{pageTitle}</h1>
-            {children}
+            <Header/>
+            <div className={mainClass.join(" ")}>
+                <h1>{pageTitle}</h1>
+                {children}
+            </div>
         </main>
     )
 }
