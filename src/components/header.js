51e93c16933e75085a492fb85f08a07f5ba74799
Jemuel Balangcod <jemuelb@gmail.com>
2021-07-24 13:43:37 -0700

    Fixed header navbar styling

--- src/components/header.js
+++ src/components/header.js
@@ -3,23 +3,32 @@ import {Link} from 'gatsby'
 
 const Header = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link
-                        to="https://www.facebook.com/groups/117966411709158/"
-                    >
-                        Facebook Group
-                    </Link>
-                </li>
-
-            </ul>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link to="/" className="navbar-brand">BIBAK San Diego</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/about">About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                className="nav-link"
+                                to="https://www.facebook.com/groups/117966411709158/"
+                            >
+                                Facebook Group
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     )
 }
