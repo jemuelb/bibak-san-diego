941a97bc0fbbc5a05cb88cd52749a2796a51fa5d
Jemuel Balangcod <jemuelb@gmail.com>
2021-07-24 13:00:26 -0700

    Removed emotion styling

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,36 +1,5 @@
 import * as React from 'react'
 import {Link, useStaticQuery, graphql} from 'gatsby'
-import {css} from '@emotion/react'
-
-const container = css`
-  margin: auto;
-  max-width: 500px;
-  font-family: sans-serif;
-`
-
-const heading = css`
-  color: rgb(219, 31, 55);
-`
-
-const navLinks = css`
-  display: flex;
-  list-style: none;
-  padding-left: 0;
-`
-
-const navLinkItem = css`
-  padding-right: 2rem;
-`
-
-const navLinkText = css`
-  color: black;
-`
-
-const siteTitle = css`
-  font-size: 2.5rem;
-  color: gray;
-  font-weight: 700;
-`
 
 const Layout = ({pageTitle, children}) => {
     const data = useStaticQuery(graphql`
@@ -44,21 +13,20 @@ const Layout = ({pageTitle, children}) => {
     `)
 
     return (
-        <main css={container}>
+        <main>
             <title>{pageTitle} | {data.site.siteMetadata.title}</title>
-            <p css={siteTitle}>{data.site.siteMetadata.title}</p>
+            <p>{data.site.siteMetadata.title}</p>
             <nav>
-                <ul css={navLinks}>
-                    <li css={navLinkItem}>
-                        <Link to="/" css={navLinkText}>Home</Link>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
                     </li>
-                    <li css={navLinkItem}>
-                        <Link to="/about" css={navLinkText}>About</Link>
+                    <li>
+                        <Link to="/about">About</Link>
                     </li>
-                    <li css={navLinkItem}>
+                    <li>
                         <Link
                             to="https://www.facebook.com/groups/117966411709158/"
-                            css={navLinkText}
                         >
                             Facebook Group
                         </Link>
@@ -66,7 +34,7 @@ const Layout = ({pageTitle, children}) => {
 
                 </ul>
             </nav>
-            <h1 css={heading}>{pageTitle}</h1>
+            <h1>{pageTitle}</h1>
             {children}
         </main>
     )
