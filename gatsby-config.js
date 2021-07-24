8d9c1a648e88418d411fb4d3f072e4ea3559c244
Jemuel Balangcod <jemuelb@gmail.com>
2021-07-24 14:17:36 -0700

    Added gatsby image plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,13 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "https://www.bibaksandiego.org",
-    title: "BIBAK San Diego",
-  },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-gatsby-cloud"],
+    siteMetadata: {
+        siteUrl: "https://www.bibaksandiego.org",
+        title: "BIBAK San Diego",
+    },
+    plugins: [
+        "gatsby-plugin-emotion",
+        "gatsby-plugin-gatsby-cloud",
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+    ],
 };
