e97c3d566c7601af259122e0235961a31d37443b
Jemuel Balangcod <jemuelb@gmail.com>
2021-07-24 18:32:32 -0700

    Fixed MDX rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,33 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import {graphql} from "gatsby"
+import {MDXRenderer} from "gatsby-plugin-mdx"
 
-const IndexPage = () => {
+const IndexPage = ({data}) => {
     return (
-        <Layout pageTitle="Home">
-            <p>Towards the northern most part of the island of luzon lies the cordillera mountain range. These
-                mountains
-                were formerly called the mountain province and were divided into five sub-provinces namely
-                benguet, ifugao,
-                bontoc, apayao, and kalinga. It was then the second largest province of the philippines.</p>
-
-            <p>In these sub-provinces are different tribes of distinct dialects and cultures, but are all also
-                similar to
-                be collectively called igorots. Perhaps it is the similarities of their cultures, or their being
-                a part of
-                the cultural minorities of the philippines, that has bonded these "People of the mountains" to
-                form an
-                organization called bibak. As simple as it is, taking the first letters of each sub-province,
-                did not only
-                make sure that this organization represents all the people of this large province, but it also
-                points to a
-                fact that each province and it's people complement each other. Together, they show a better
-                image of an
-                igorot.</p>
-
-            <p>On june 18, 1966, republic act 4695 divided the province into four provinces, benguet, ifugao,
-                mountain
-                province (bontoc was named as its capital), and kalinga-apayao. This caused some bibak
-                organizations to
-                change their name to bimak to reflect the change of name but others preferred to stay with
-                bibak. The
-                formation of the cordillera administration region, which added the province of abra to the four
-                provinces,
-                has also prompted the formation of some organizations called bimaak. But through it all, name
-                change or not,
-                bibak and the others are organizations of igorots. Bibak is an organization that reflects the
-                solidarity and
-                desire to preserve the uniqueness of the igorot culture.</p>
-            <p>
-                Join us in our active <a href="https://www.facebook.com/groups/117966411709158/">Facebook Group</a>
-            </p>
+        <Layout pageTitle={data.mdx.frontmatter.title}>
+            <article key={data.mdx.id}>
+                <MDXRenderer>
+                    {data.mdx.body}
+                </MDXRenderer>
+                <p>Last Updated: {data.mdx.frontmatter.date}</p>
+            </article>
         </Layout>
     )
 }
 
 export default IndexPage
+
+export const query = graphql`
+    query {
+        mdx(slug: {eq: "main"}) {
+            frontmatter {
+                title
+                date
+            }
+            body
+            id
+            slug
+        }
+    }
+`
