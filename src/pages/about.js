c9d9dbbf6afb769cfa9c380fc5388d7fec146e48
Jemuel Balangcod <jemuelb@gmail.com>
2021-07-24 18:39:37 -0700

    Converted about to use MDX

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,48 +1,33 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import {graphql} from "gatsby"
+import {MDXRenderer} from "gatsby-plugin-mdx"
 
-const AboutPage = () => {
+const AboutPage = ({data}) => {
     return (
-        <Layout pageTitle="About">
-            <h3>BIBAK San Diego Through the Years</h3>
-            <p>Entering the decade of the 80's, some Igorots have already established themselves in San Diego. They
-                may be
-                military families who took San diego as their duty station, or civilians who decided to make
-                this beautiful
-                city their home. As often the case with people, they try to seek other people who share the same
-                traditions
-                and cultures, but up to this time, only a handful of Igorot families were getting together
-                during special
-                occasions. For Andy Afuyog and James Ambasing, who first met at San Diego City College, it was
-                apparent that
-                there was a need for Igorots to reach out to other Igorots not only to help each other, but to
-                socialize and
-                know more kailians. From here the idea of a BIBAK San Diego was conceived and from that
-                conception, came the
-                birth of an organization.</p>
-
-            <p>On January 18, 1981, the first election of BIBAK San Diego Officers was held at the residence of Felix
-                and
-                Rose Lucaben. Present during this historic moment were Andy andLinda Afuyog, James and Cres Ambasing,
-                John
-                and Alice Baon, Orlando and Edith Esteban, Felissa Estepa,Maricana Domilos, Edmund and Marilyn Garcia,
-                Johnand Luz Pulsingay, and Hilaria Sendong. Induction of the first elected officers was held March, 1981
-                and
-                has been designated as BIBAK San Diego's Foundation Month. The first officers were: President: Jonh
-                Pulsingay;Vice President: Felix Lucaben; Secretary/ Treasurer: Domitilla Comenat; Auditor: Edwin Abeya;
-                and
-                PRO: Andres Afuyog. Members and officers have come and gone since then, and BIBAK San Diego has steadily
-                grown. If you live near the area, please contact us and be a member. If you happen to visit San Diego,
-                rest
-                assured that there are Igorots here to welcome you. (Charlie Tican, 1997)</p>
-            <h3>Notable Events</h3>
-            <ul>
-                <li>1982 - Founding of BIBAK San Diego in March</li>
-                <li>2008 - First Grand Canao "The Beat Goes On"</li>
-                <li>2014 - Second Grand Canao "The Beat Goes On II"</li>
-            </ul>
+        <Layout pageTitle={data.mdx.frontmatter.title}>
+            <article key={data.mdx.id}>
+                <MDXRenderer>
+                    {data.mdx.body}
+                </MDXRenderer>
+                <p>Last Updated: {data.mdx.frontmatter.date}</p>
+            </article>
         </Layout>
     )
 }
 
 export default AboutPage
+
+export const query = graphql`
+    query {
+        mdx(slug: {eq: "about"}) {
+            frontmatter {
+                title
+                date
+            }
+            body
+            id
+            slug
+        }
+    }
+`
