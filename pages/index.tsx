2229f932bff3260fcdcf4e853f4566450ee78b8b
Jemuel Balangcod <jemuelb@gmail.com>
2023-03-19 21:14:04 -0700

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>BIBAK San Diego</title>
+                <link rel="icon" href="/favicon.ico"/>
+            </Head>
+            <main>
+              <h1>BIBAK San Diego</h1>
+                <div>
+                    Landing Page
+                </div>
+            </main>
+        </>
+    )
+}
