ded568b5aec3ef2fa7cd6ac095a048ee5ce948e6
Jemuel Balangcod <jemuelb@gmail.com>
2023-03-20 20:49:40 -0700

    Added example pages with buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,10 @@ const Home = () => {
                         <div className="container-fluid py-5">
                             <h1 className="display-5 fw-bold">Grand Canao 2023</h1>
                             <p className="col-md-8 fs-4">September 1 - 3, Town & Country, San Diego, CA</p>
-                            <button className="btn btn-primary btn-lg" type="button">Event Details</button>
+                            <Link href="/event-details">
+                                <button className="btn btn-primary btn-lg" type="button">Event Details</button>
+                            </Link>
+
                         </div>
                     </div>
 
@@ -29,7 +32,9 @@ const Home = () => {
                             <div className="h-100 p-5 text-bg-dark rounded-3">
                                 <h2>Things to do...</h2>
                                 <p>More content about the </p>
-                                <button className="btn btn-outline-light" type="button">Example button</button>
+                                <Link href="/additional-info">
+                                    <button className="btn btn-outline-light" type="button">More info</button>
+                                </Link>
                             </div>
                         </div>
                         <div className="col-md-6">
@@ -37,7 +42,9 @@ const Home = () => {
                                 <h2>About us</h2>
                                 <p>Something something, about the details talking about BIBAK San Diego, the
                                     organization</p>
-                                <button className="btn btn-outline-secondary" type="button">Example button</button>
+                                <Link href="/about">
+                                    <button className="btn btn-outline-secondary" type="button">More...</button>
+                                </Link>
                             </div>
                         </div>
                     </div>
