60247d81abdb3c6e987f4350d54530109a05e44a
Jemuel Balangcod <jemuelb@gmail.com>
2023-03-20 20:40:06 -0700

    Updated the layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import {Container, Row, Col} from 'react-bootstrap';
+import Link from 'next/link'
+import {Container} from 'react-bootstrap';
 
 export default function Home() {
     return (
@@ -9,16 +10,38 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
             <main>
-              <Container>
-                  <Row>
-                      <h1>BIBAK San Diego</h1>
-                  </Row>
-                  <Row>
-                      <Col>
-                          This is a temporary holding page.
-                      </Col>
-                  </Row>
-              </Container>
+                <Container>
+                    <header className="pb-3 mb-4 border-bottom">
+                        <a href="/" className="d-flex align-items-center text-dark text-decoration-none">
+                            <span className="fs-4">BIBAK San Diego</span>
+                        </a>
+                    </header>
+                    <div className="p-5 mb-4 bg-light rounded-3">
+                        <div className="container-fluid py-5">
+                            <h1 className="display-5 fw-bold">Grand Canao 2023</h1>
+                            <p className="col-md-8 fs-4">September 1 - 3, Town & Country, San Diego, CA</p>
+                            <button className="btn btn-primary btn-lg" type="button">Event Details</button>
+                        </div>
+                    </div>
+
+                    <div className="row align-items-md-stretch">
+                        <div className="col-md-6">
+                            <div className="h-100 p-5 text-bg-dark rounded-3">
+                                <h2>Things to do...</h2>
+                                <p>More content about the </p>
+                                <button className="btn btn-outline-light" type="button">Example button</button>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="h-100 p-5 bg-light border rounded-3">
+                                <h2>About us</h2>
+                                <p>Something something, about the details talking about BIBAK San Diego, the
+                                    organization</p>
+                                <button className="btn btn-outline-secondary" type="button">Example button</button>
+                            </div>
+                        </div>
+                    </div>
+                </Container>
             </main>
         </>
     )
