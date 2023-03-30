024224f2db50693bd119ca48b2cc28d70f1296d4
Jemuel Balangcod <jemuelb@gmail.com>
2023-03-30 05:22:34 -0700

    Changed pages to use the layout component

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,27 +1,14 @@
-import Head from "next/head";
-import {Container} from "react-bootstrap";
 import Link from "next/link";
+import Layout from "@/components/Layout";
 
 const AboutPage = () => {
     return (
-        <>
-            <Head>
-                <title>BIBAK San Diego</title>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-            <main>
-                <Container>
-                    <header className="pb-3 mb-4 border-bottom">
-                        <Link href="/" className="d-flex align-items-center text-dark text-decoration-none">
-                            <span className="fs-4">BIBAK San Diego</span>
-                        </Link>
-                    </header>
-                    <div>
-                        <h1>About Page</h1>
-                    </div>
-                </Container>
-            </main>
-        </>
+        <Layout title={'About BIBAK San Diego'}>
+            <div>
+                <h1>About Page</h1>
+                <Link href='/'>Go Back</Link>
+            </div>
+        </Layout>
     )
 }
 
--- pages/event-details.tsx
+++ pages/event-details.tsx
@@ -1,27 +1,14 @@
-import Head from "next/head";
-import {Container} from "react-bootstrap";
 import Link from "next/link";
+import Layout from "@/components/Layout";
 
 const EventDetails = () => {
     return (
-        <>
-            <Head>
-                <title>BIBAK San Diego</title>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-            <main>
-                <Container>
-                    <header className="pb-3 mb-4 border-bottom">
-                        <Link href="/" className="d-flex align-items-center text-dark text-decoration-none">
-                            <span className="fs-4">BIBAK San Diego</span>
-                        </Link>
-                    </header>
-                    <div>
-                        <h1>Details Page</h1>
-                    </div>
-                </Container>
-            </main>
-        </>
+        <Layout title={'Event Details'}>
+            <div>
+                <h1>Event Details Page</h1>
+                <Link href='/'>Go Back</Link>
+            </div>
+        </Layout>
     )
 }
 
