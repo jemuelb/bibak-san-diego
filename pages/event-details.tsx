9974ea57e31fe4d4e3640cf887cb6e37ca796262
Jemuel Balangcod <jemuelb@gmail.com>
2023-03-30 05:43:40 -0700

    Added raw data to the page

--- pages/event-details.tsx
+++ pages/event-details.tsx
@@ -1,15 +1,50 @@
 import Link from "next/link";
 import Layout from "@/components/Layout";
+import {Col, Row} from "react-bootstrap";
 
 const EventDetails = () => {
     return (
         <Layout title={'Event Details'}>
-            <div>
-                <h1>Event Details Page</h1>
-                <Link href='/'>Go Back</Link>
-            </div>
+            <Row>
+                <Col>
+                    <h1>Grand Canao</h1>
+                    <h3>The Beat Must Go On</h3>
+
+                    <div>
+                        September 2, 2023
+                        Town and Country San Diego
+                        500 Hotel Circle North
+                        San Diego, CA 92108
+                    </div>
+                    <div>
+                        Tickets: $90 per person
+                        $45 12 years old & below
+                    </div>
+                    <div>
+                        Additional Events:
+                        Welcome Party
+                        5:00 PM, September 1, 2023
+                        1852 Camino Mojave
+                        Chula Vista, CA 91914
+                    </div>
+                    <div>
+                        Send Off Party
+                        12:00 PM, September 3, 2023
+                        Rhor Park, 4548 Sweetwater Rd
+                        Bonita, CA 91902
+                    </div>
+                    <div>
+                        Contact for more information:
+                        Carlo J. Basallo - Carlene07@hotmail.com
+                        Kaila Domingo - domingokaila@gmail.com
+                        Kirin Macapugay - kirin568@gmail.com
+                        Jackie Fianza - jackie.fianza@gmail.com
+                    </div>
+                    <Link href='/'>Go Back</Link>
+                </Col>
+            </Row>
         </Layout>
     )
-}
+};
 
 export default EventDetails;
