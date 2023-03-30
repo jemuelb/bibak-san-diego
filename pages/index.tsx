0dbe5f5c2a8a06b46429e14e464ce08298bd7086
Jemuel Balangcod <jemuelb@gmail.com>
2023-03-30 05:34:43 -0700

    Updated front page content

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,20 +19,17 @@ const Home = () => {
                 <div className="row align-items-md-stretch">
                     <div className="col-md-6">
                         <div className="h-100 p-5 text-bg-dark rounded-3">
-                            <h2>Things to do...</h2>
-                            <p>More content about the </p>
-                            <Link href="/additional-info">
-                                <button className="btn btn-outline-light" type="button">More info</button>
-                            </Link>
+                            <h2>Visitor Guide</h2>
+                            <p>For visitors coming outside of the San Diego area, more details coming soon...</p>
+                            <button className="btn btn-outline-light" type="button" disabled={true}>Coming Soon</button>
                         </div>
                     </div>
                     <div className="col-md-6">
                         <div className="h-100 p-5 bg-light border rounded-3">
                             <h2>About us</h2>
-                            <p>Something something, about the details talking about BIBAK San Diego, the
-                                organization</p>
-                            <Link href="/about">
-                                <button className="btn btn-outline-secondary" type="button">More...</button>
+                            <p>Click the link below to view the BIBAK San Diego Facebook group</p>
+                            <Link href="https://www.facebook.com/groups/117966411709158">
+                                <button className="btn btn-outline-secondary" type="button">Facebook Group</button>
                             </Link>
                         </div>
                     </div>
