7cdaa1cb47c225e80a0223ab38a30cd34ded07c8
Jemuel Balangcod <jemuelb@gmail.com>
2023-03-31 05:46:04 -0700

    Restructured detail page

--- pages/event-details.tsx
+++ pages/event-details.tsx
@@ -7,28 +7,32 @@ const EventDetails = () => {
         <Layout title={'Event Details'}>
             <Row>
                 <Col>
-                    <div>
-                        <div className="text-center">
-                            <h1>Grand Canao</h1>
-                        </div>
-                        <div className="text-center">
-                            <h3>The Beat Must Go On</h3>
-                        </div>
+                    <div className="text-center">
+                        <h1>Grand Canao</h1>
                     </div>
+                    <div className="text-center">
+                        <h3>The Beat Must Go On</h3>
+                    </div>
+                    <div className="text-center">September 2, 2023</div>
                 </Col>
             </Row>
-            <Row>
+            <Row className="mb-3">
                 <Col>
-                    <div>
-                        September 2, 2023
-                        Town and Country San Diego
-                        500 Hotel Circle North
-                        San Diego, CA 92108
-                    </div>
-                    <div>
-                        Tickets: $90 per person
-                        $45 12 years old & below
-                    </div>
+                    <address>
+                        <div>Town and Country San Diego</div>
+                        <div>500 Hotel Circle North</div>
+                        <div>San Diego, CA 92108</div>
+                    </address>
+                </Col>
+            </Row>
+            <Row className="mb-3">
+                <Col>
+                    Tickets: $90 per person
+                    $45 12 years old & below
+                </Col>
+            </Row>
+            <Row className="mb-3">
+                <Col>
                     <div>
                         Additional Events:
                         Welcome Party
@@ -36,19 +40,31 @@ const EventDetails = () => {
                         1852 Camino Mojave
                         Chula Vista, CA 91914
                     </div>
+                </Col>
+                <Col>
                     <div>
                         Send Off Party
                         12:00 PM, September 3, 2023
                         Rhor Park, 4548 Sweetwater Rd
                         Bonita, CA 91902
                     </div>
+                </Col>
+            </Row>
+            <Row className="mb-3">
+                <Col>
                     <div>
-                        Contact for more information:
+                        Contact information
+                    </div>
+                    <div>
                         Carlo J. Basallo - Carlene07@hotmail.com
                         Kaila Domingo - domingokaila@gmail.com
                         Kirin Macapugay - kirin568@gmail.com
                         Jackie Fianza - jackie.fianza@gmail.com
                     </div>
+                </Col>
+            </Row>
+            <Row className="mb-3">
+                <Col>
                     <div>
                         <a href='/flyer.jpg'>Link to the flyer</a>
                     </div>
