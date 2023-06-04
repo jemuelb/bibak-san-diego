95452d5ada804edbbf478e4d43cc39c51bf5982d
Jemuel Balangcod <jemuelb@gmail.com>
2023-06-04 04:54:12 -0700

    Removed Head element

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,10 +8,6 @@ export default function App(props: AppProps) {
 
     return (
         <>
-            <Head>
-                <title>BIBAK San Diego</title>
-                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-            </Head>
             <MantineProvider
                 withGlobalStyles
                 withNormalizeCSS
