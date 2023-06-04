c0695c9acd7afc064919c35305f8b7a394924149
Jemuel Balangcod <jemuelb@gmail.com>
2023-06-04 04:51:48 -0700

    Initialized mantine UI in next components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-icons/font/bootstrap-icons.css'
 import type {AppProps} from 'next/app'
 import {Analytics} from '@vercel/analytics/react';
+import {MantineProvider} from "@mantine/core";
+import Head from "next/head";
 
-export default function App({Component, pageProps}: AppProps) {
+export default function App(props: AppProps) {
+    const {Component, pageProps} = props;
+
     return (
         <>
-            <Component {...pageProps} />
+            <Head>
+                <title>BIBAK San Diego</title>
+                <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+            </Head>
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{
+                    colorScheme: 'light',
+                }}
+            >
+                <Component {...pageProps} />
+            </MantineProvider>
             <Analytics/>
         </>
     );
